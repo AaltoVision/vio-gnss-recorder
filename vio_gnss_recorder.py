dbefe9d9b8c310e353bd503b980e93c754039239
Eetu Pakkanen <37110871+randomEetu@users.noreply.github.com>
2023-07-20 17:21:22 +0300

    Create config.ini and python script
    Create configuration file to define relevant file paths
    
    Ask credentials, IP address etc. Only coordinates are checked for correct format. We could check that for example user name and mountpoint don't include whitespace but at this point it's probably not necessary

--- vio_gnss_recorder.py
+++ vio_gnss_recorder.py
@@ -10,6 +10,9 @@ Running the script requires an internet connnection for RTK's correction data an
 
 """
 
+import getpass
+import readline
+
 import configparser
 
 # 'Define' the absolute paths of the directories this script depends on
@@ -18,4 +21,29 @@ configuration.read("config.ini")
 U_BLOX_CAPTURE_PATH = configuration['Filepaths']['u-blox-capture']
 SDK_EXAMPLES_PATH = configuration['Filepaths']['sdk-examples']
 
+# Is the user logged in as sudo?
+print("\nYou are logged in as " + getpass.getuser() + "\n")
+
+### Ask credentials, mountpoint address, rough coordinates...
+print("Your RTK providers credentials:")
+user = input("Username: ")
+password = getpass.getpass()
+print("\n")
+address = input("RTK provider's IP address eg. 'xyz.abc.com': ")
+port = input("Port: ")
+mountpoint = input("Specify the used mountpoint: ")
+
+# Validate correct format for coordinates, accept '.' and '-'
+while True:
+    wrongFormat = "Wrong format. Try again using the correct format, like this: 60.2 24.8"
+    try:
+        lat, lon = input("Type the coordinates of your current location in format 'LAT LON' (use . as a decimal separator):\n").split()
+        if not float(lat) and not float(lon):
+            print(wrongFormat)
+            continue
+    except ValueError:
+        print(wrongFormat)
+        continue
+    else:
+        break
 
